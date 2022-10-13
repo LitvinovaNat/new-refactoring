a286f5c78d6d3d5b731eac1ef68bd69d5700988c
natli <77048533+LitvinovaNat@users.noreply.github.com>
2022-10-13 21:51:38 +0700

    Adding new ver

--- first/first/Program.cs
+++ first/first/Program.cs
@@ -7,35 +7,32 @@ namespace first
         {
             public string FirstName { get; protected set; }
             public string LastName { get; protected set; }
-            public virtual string GetSex() => "Unknow";
 
-            public Person(string firstName, string lastName)
+
+            private string Sex = "Unknow";
+            public virtual string GetSex() => Sex;
+
+        private Person(string firstName, string lastName)
             {
                 FirstName = firstName;
                 LastName = lastName;
             }
 
+        protected Person(string firstName, string lastName, string sex) : this(firstName, lastName) => Sex = sex;
 
-        }
+        public static Person CreateMan(string firstName, string lastName) => new Person(firstName, lastName, "Man");
 
-        public class Man : Person
-        {
-            public Man(string firstName, string lastName) : base(firstName, lastName) { }
-            public override string GetSex() => "Man";
-        }
+        public static Person CreateWoman(string firstName, string lastName) => new Person(firstName, lastName, "Woman");
 
-        public class Woman : Person
-        {
-            public Woman(string firstName, string lastName) : base(firstName, lastName) { }
-            public override string GetSex() => "Woman";
-        }
+
+    }
 
         public class Program
         {
             public static void Main(string[] args)
             {
-                Person woman = new Woman("Elena", "Kovolenko");
-                Person man = new Man("Nikita", "Smirnov");
+                Person woman = Person.CreateWoman("Elena", "Kovolenko");
+                Person man = Person.CreateMan("Nikita", "Smirnov");
                 string sex = woman.GetSex();
                 Console.WriteLine(sex);
                 Console.ReadLine();
